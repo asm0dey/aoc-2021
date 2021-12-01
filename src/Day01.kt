58cfcdf0402c5a3b01d3b2df6447fe8551157c9a
asm0dey <pavel.finkelshtein@gmail.com>
2021-12-01 10:12:59 +0300

    Adds optimized solution of part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -16,11 +16,22 @@ fun main() {
             .count { it[1] > it[0] }
     }
 
+    fun part2Optimized(input:List<String>):Int{
+        return input
+            .map { it.toInt() }
+            .windowed(4)
+            .count { it[3] > it[0] }
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
+
+    check(part2(testInput) == 5)
     println(part2(input))
+
+    check(part2(input)==part2Optimized(input))
 }
