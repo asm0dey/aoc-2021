718f8668d35f1e6d667e1a2d948e4e0423ebc3c6
asm0dey <pavel.finkelshtein@gmail.com>
2021-12-07 08:52:54 +0300

    Adds Day 7

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,13 +1,13 @@
 fun main() {
     fun solve(input: List<String>, iterNum: Int): Long {
-        val timers = ArrayDeque<Long>(List(9) { 0 })
-
+        val timers = ArrayDeque(List(9) { 0L })
         for (fish in input.first().split(","))
             timers[fish.toInt()]++
 
         repeat(iterNum) {
             timers.addLast(timers.removeFirst())
             timers[6] += timers[8]
+            println(timers)
         }
         return timers.sum()
     }
---
+++ src/Day07.kt
@@ -0,0 +1,33 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val positions = input.first().split(',').map { it.toInt() }.sorted()
+        return (positions.first()..positions.last())
+            .asSequence()
+            .map { position ->
+                positions.sumOf { abs(it - position) }
+            }
+            .minOf { it }
+    }
+
+    fun part2(input: List<String>): Int {
+        val positions = input.first().split(',').map { it.toInt() }.sorted()
+        return (positions.first()..positions.last())
+            .asSequence()
+            .map { position ->
+                positions.map { abs(position - it) }.sumOf { (it + 1) * it / 2 }
+            }
+            .minOf { it }
+    }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 37)
+    val input = readInput("Day07")
+    println(part1(input))
+    check(part2(testInput) == 168)
+    println(part2(input))
+}
+
