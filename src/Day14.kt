969a1315bce17493aaadfabb48cf719f4e4e8bb1
asm0dey <pavel.finkelshtein@gmail.com>
2021-12-14 14:50:56 +0300

    Day 14: pure functional solution

--- src/Day14.kt
+++ src/Day14.kt
@@ -8,22 +8,24 @@ fun main() {
         return Pair(source, replacements)
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>, iterations: Int): Int {
         val (source, replacements) = parseInput(input)
-        val result = (0..9).fold(source) { it, _ ->
-            it
-                //  ABC
-                //      AXB BYC
-                //          AX BY C
-                .windowed(2, partialWindows = true) {
-                    if (it.length == 2) {
-                        if (replacements.contains(it.toString())) "${it[0]}${replacements[it.toString()]}"
-                        else it.toString()
-                    } else it
-                }
-                .joinToString("")
-        }
-        val freqs = result.groupingBy { it }.eachCount().values
+        val freqs = (0 until iterations)
+            .fold(source) { it, _ ->
+                it
+                    //  ABC
+                    //      AXB BYC
+                    //          AX BY C
+                    .windowed(2, partialWindows = true) {
+                        if (it.length == 2)
+                            "${it[0]}${replacements[it.toString()]}"
+                        else it
+                    }
+                    .joinToString("")
+            }
+            .groupingBy { it }
+            .eachCount()
+            .values
         val max = freqs.maxOf { it }
         val min = freqs.minOf { it }
         return max - min
@@ -39,52 +41,42 @@ fun main() {
 
     fun part2(input: List<String>, iterations: Int): Long {
         val (source, replacements) = parseInput(input)
-        val final = (0 until iterations).fold(countPairs(source)) { map, _ ->
-            val next = mutableMapOf<String, Long>()
-            for ((pair, count) in map) {
-                if (replacements.contains(pair)) {
-                    val newChar = replacements[pair]!!
-                    val fst = pair[0] + newChar
-                    val snd = newChar + pair[1]
-                    for (newPair in listOf(fst, snd)) {
-                        next.compute(newPair) { _, cur ->
-                            if (cur == null) count
-                            else cur + count
-                        }
+        val freqs = (0 until iterations)
+            .fold(countPairs(source)) { map, _ ->
+                map
+                    .flatMap { (symbolPair, count) ->
+                        val newChar = replacements[symbolPair]!!
+                        val fst = symbolPair[0] + newChar
+                        val snd = newChar + symbolPair[1]
+                        listOf(fst to count, snd to count)
                     }
-                }
+                    .groupBy { it.first }
+                    .mapValues { (_, counts) -> counts.sumOf { it.second } }
             }
-            next
-        }
-        val rawFreqs = hashMapOf<Char, Long>()
-        for ((pair, count) in final) {
-            for (char in pair) {
-                rawFreqs.compute(char) { _, cur ->
-                    if (cur == null) count
-                    else cur + count
-                }
+            .flatMap { (pair, count) -> pair.map { it to count } }
+            .groupBy { (char, _) -> char }
+            .mapValues { (_, listCounts) -> listCounts.sumOf { (_, count) -> count } }
+            .mapValues { (char, count) ->
+                //  AN
+                //    AX XN    (AXN)
+                //        AA AX XB BX XN NN (AAXBXNN)
+                if (char == source.first() || char == source.last()) (count + 1) / 2
+                else count / 2
             }
-        }
-        val freqs = rawFreqs.mapValues { (char, count) ->
-            //  AN
-            //    AX XN    (AXN)
-            //        AA AX XB BX XN NN (AAXBXNN)
-            if (char == source.first() || char == source.last()) (count + 1) / 2
-            else count / 2
-        }
-        val max = freqs.maxOf { it.value }
-        val min = freqs.minOf { it.value }
+            .values
+        val max = freqs.maxOf { it }
+        val min = freqs.minOf { it }
         return max - min
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day14_test")
-    check(part1(testInput) == 1588)
-    println(part1(testInput))
+    check(part1(testInput, 10) == 1588)
+    println(part1(testInput, 10))
     println(part2(testInput, 10))
 
     val input = readInput("Day14")
-    println(part1(input))
+    println(part1(input, 10))
     println(part2(input, 10))
 
     println(part2(input, 40))
