3c8050f38da7c2188b5890fabd6fca6616b125a1
asm0dey <pavel.finkelshtein@gmail.com>
2021-12-25 00:55:13 +0300

    Move common predicates to variables

--- src/Day24.kt
+++ src/Day24.kt
@@ -1,9 +1,9 @@
-
 // This solution only works for my input: I've converted the source to Kotlin with search and replace and then refactored it heavily
 // to leave predicate in separate variables. Each predicate should be equal 0 for z to not grow
 fun main() {
-    fun run(i1: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int,
-            i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int
+    fun run(
+        i1: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int,
+        i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int,
     ): Int {
         var z: Int = i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750
         var x = z
@@ -27,12 +27,14 @@ fun main() {
         val b5 = if (x % 26 - 8 == i11) 0 else 1
         z = z / 26 * 25 * b5 + z / 26
         z += (i11 + 6) * b5
-        x = z // ((((((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750)/26)/26)/26) * 26 + i8 + 13) * 26 + i9 + 16)/26)/26)
+        x =
+            z // ((((((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750)/26)/26)/26) * 26 + i8 + 13) * 26 + i9 + 16)/26)/26)
         val b6 = if (x % 26 - 11 == i12) 0 else 1
         z = z / 26 * 25 * b6 + z / 26
         z += (i12 + 9) * b6
         z = z * 26 + i13 + 11
-        x = z // ((((((((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750)/26)/26)/26) * 26 + i8 + 13) * 26 + i9 + 16)/26)/26)) * 26 + i13 + 11)
+        x =
+            z // ((((((((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750)/26)/26)/26) * 26 + i8 + 13) * 26 + i9 + 16)/26)/26)) * 26 + i13 + 11)
         val b7 = if (x % 26 - 15 == i14) 0 else 1
         z = z / 26 * 25 * b7 + z / 26
         z += (i14 + 5) * b7
@@ -41,20 +43,37 @@ fun main() {
 
 
     val results = arrayListOf<String>()
-    for (i1 in 1..9) for (i2 in 1..9) for (i3 in 1..9) for (i4 in 1..9) for (i5 in 1..9)
-        if (i5 == (i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750) % 26 - 16)
-            for (i6 in 1..9) if (i6 == ((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750) / 26) % 26 - 11)
-                for (i7 in 1..9) if ((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750) / 26) / 26) % 26 - 6 == i7)
-                    for (i8 in 1..9) for (i9 in 1..9) for (i10 in 1..9)
-                        if (((((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750) / 26) / 26) / 26) * 26 + i8 + 13) * 26 + i9 + 16) % 26 - 10 == i10)
-                            for (i11 in 1..9)
-                                if ((((((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750) / 26) / 26) / 26) * 26 + i8 + 13) * 26 + i9 + 16) / 26) % 26 - 8 == i11)
-                                    for (i12 in 1..9)
-                                        if (((((((((i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750) / 26) / 26) / 26) * 26 + i8 + 13) * 26 + i9 + 16) / 26) / 26) % 26 - 11 == i12)
-                                            for (i13 in 1..9)
-                                                for (i14 in 1..9)
-                                                    if (run(i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14) == 0)
-                                                        results.add("$i1$i2$i3$i4$i5$i6$i7$i8$i9$i10$i11$i12$i13$i14")
+    for (i1 in 1..9)
+        for (i2 in 1..9)
+            for (i3 in 1..9)
+                for (i4 in 1..9)
+                    for (i5 in 1..9) {
+                        val p1 = i1 * 17576 + i2 * 676 + i3 * 26 + i4 + 97750
+                        if (i5 == p1 % 26 - 16)
+                            for (i6 in 1..9) {
+                                val p2 = p1 / 26
+                                if (i6 == p2 % 26 - 11)
+                                    for (i7 in 1..9) {
+                                        val p3 = p2 / 26
+                                        if (p3 % 26 - 6 == i7)
+                                            for (i8 in 1..9)
+                                                for (i9 in 1..9)
+                                                    for (i10 in 1..9) {
+                                                        val p4 = ((p3 / 26) * 26 + i8 + 13) * 26 + i9 + 16
+                                                        if (p4 % 26 - 10 == i10)
+                                                            for (i11 in 1..9)
+                                                                if ((p4 / 26) % 26 - 8 == i11)
+                                                                    for (i12 in 1..9)
+                                                                        if (((p4 / 26) / 26) % 26 - 11 == i12)
+                                                                            for (i13 in 1..9)
+                                                                                for (i14 in 1..9)
+                                                                                    if (((((p4 / 26) / 26)) * 26 + i13 + 11) % 26 - 15 == i14)
+                                                                                        if (run(i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14) == 0)
+                                                                                            results.add("$i1$i2$i3$i4$i5$i6$i7$i8$i9$i10$i11$i12$i13$i14")
+                                                    }
+                                    }
+                            }
+                    }
     println(results.last())
     println(results.first())
 }
